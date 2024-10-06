eabae0a3f6631ab0eec44e7fd3fb13f5494f81a9
Legonius <emoinblackzone@gmail.com>
2024-10-07 00:22:50 +0630

    upload

---
+++ Episode1/Inception.js
@@ -0,0 +1,57 @@
+//Vanilla Javascript Way of Create & Append Element
+
+// const root = document.getElementById("root");
+// const heading = document.createElement("h1");
+// heading.textContent = "Hello World";
+// root.appendChild(heading);
+
+// Same Logic with React Js
+
+const headig = React.createElement(
+  "h1",
+  { key: "heading", id: "h1" },
+  "Namaste React"
+);
+
+const para = React.createElement("p", { key: "1" }, "This is First Paragraph");
+const para2 = React.createElement(
+  "p",
+  { key: "2" },
+  "This is Second Paragraph"
+);
+const body = React.createElement("div", { key: "body", className: "body" }, [
+  para,
+  para2,
+]);
+const footer = React.createElement(
+  "footer",
+  { key: "footer", id: "footer" },
+  "Bye Bye"
+);
+
+const app = React.createElement(
+  "div", //type
+  {
+    className: "main",
+    style: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+      alignItems: "center",
+      height: "100vh",
+    },
+  }, //props (attribute)
+  [headig, body, footer] // children
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(app);
+
+/*
+
+//props
+  "h1", //element
+  { className: "headings", style: { fontSize: "5rem" } }, //attrributes
+  "Hello World from React" //children
+
+
+  */
