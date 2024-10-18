87c7ff2a4f74050178d359324c88f6a081918e37
Legonius <emoinblackzone@gmail.com>
2024-10-18 23:13:46 +0630

    clean up code

--- Episode13/Src/Components/__test__/card.test.js
+++ Episode13/Src/Components/__test__/card.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Card from "../Card";
+import Card, { higherOrderComponentCard } from "../Card";
 import MOCK_DATA from "../Mock/mockdata.json";
 
 describe("Should render card with props", () => {
@@ -11,26 +11,6 @@ describe("Should render card with props", () => {
     expect(title).toBeInTheDocument();
   });
   it("Should render all details getting as props object with recommended tag", () => {
-    const higherOrderComponentCard = (Card) => {
-      return (props) => {
-        return (
-          <div className=" relative">
-            <div
-              style={{
-                backgroundColor: "gray",
-                color: "white",
-                position: "absolute",
-                borderRadius: "5px",
-                padding: "2px 4px",
-              }}
-            >
-              recommended
-            </div>
-            <Card {...props} />
-          </div>
-        );
-      };
-    };
     const RecommendedCard = higherOrderComponentCard(Card);
     render(<RecommendedCard obj={MOCK_DATA} />);
     const recommended = screen.getByText("recommended");
